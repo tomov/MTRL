f44f4b702410806e742140fdd37587ceb51c4f6d
Momchil Tomov <tomov90@gmail.com>
2019-02-19 21:18:04 -0500

    SF works too!

--- demo.m
+++ demo.m
@@ -41,6 +41,7 @@ gamma = 0.99;
 %
 for t = 1:length(w_train)
     V{t} = zeros(1, env.N);
+    pi{t} = zeros(1, env.N);
 
     fprintf('t = %d\n', t);
 
@@ -62,7 +63,10 @@ for t = 1:length(w_train)
             for a = env.A
                 tmp = r + sum(squeeze(env.T(s, a, :))' .* (gamma * V{t}));
                 fprintf('           a = %d, tmp = %f\n', s, tmp);
-                best = max(best, tmp);
+                if best < tmp
+                    best = tmp;
+                    pi{t}(s) = a;
+                end
             end
             V{t}(s) = best;
 
@@ -78,37 +82,74 @@ end
 
 % train UVFA
 %
-X = [];
-y = [];
-for t = 1:length(w_train)
-    for s = env.S
-        s_onehot = zeros(1, env.N);
-        s_onehot(s) = 1;
-        x = [s_onehot w_train{t}];
-        X = [X; x];
-        y = [y; V{t}(s)];
-    end
-end
-X = repmat(X, 10, 1);
-y = repmat(y, 10, 1);
-X = X'; % UGH matlab
-y = y'; 
-
-UVFA = fitnet(10);
-UVFA = train(UVFA, X, y);
-
-yy = UVFA(X);
-perf = perform(UVFA, yy, y);
-perf
-
-
-
-% see what it will do
+%X = [];
+%y = [];
+%for t = 1:length(w_train)
+%    for s = env.S
+%        s_onehot = zeros(1, env.N);
+%        s_onehot(s) = 1;
+%        x = [s_onehot w_train{t}];
+%        X = [X; x];
+%        y = [y; V{t}(s)];
+%    end
+%end
+%X = repmat(X, 10, 1);
+%y = repmat(y, 10, 1);
+%X = X'; % UGH matlab
+%y = y'; 
 %
-V_test = zeros(1, env.N);
-for s = 2:4
-    s_onehot = zeros(1, env.N);
-    s_onehot(s) = 1;
-    x = [s_onehot w_test{1}];
-    V_test(s) = UVFA(x');
+%UVFA = fitnet(10);
+%UVFA = train(UVFA, X, y);
+%
+%yy = UVFA(X);
+%perf = perform(UVFA, yy, y);
+%perf
+%
+%
+%
+%% see what it will do
+%%
+%for t = 1:length(w_test)
+%    V_test{t} = zeros(1, env.N);
+%    for s = 1:env.N
+%        s_onehot = zeros(1, env.N);
+%        s_onehot(s) = 1;
+%        x = [s_onehot w_test{t}];
+%        V_test{t}(s) = UVFA(x');
+%    end
+%end
+%
+
+% get SFs using iteration
+%
+for t = 1:length(w_train)
+    fprintf('t = %d\n', t);
+
+    % important to init at least terminal states
+    for s = 1:env.N
+        psi{t}{s} = env.phi{s};
+    end
+
+    threshold = 0.01;
+    while true
+        delta = 0;
+        for s = env.S
+            fprintf('   s = %d\n', s);
+            old = psi{t}{s};
+
+            a = pi{t}(s);
+
+            psi{t}{s} = env.phi{s};
+            for s_new = 1:env.N
+                psi{t}{s} = psi{t}{s} + env.T(s, a, s_new) * gamma * psi{t}{s_new};
+                fprintf('          a = %d, s_new = %d, T(s,a,s_new) = %f, psi{t}{s_new} = [%f %f %f]\n', a, s_new, env.T(s, a, s_new), psi{t}{s_new});
+            end
+
+            delta = max(delta, norm(old - psi{t}{s}));
+        end
+
+        if delta < threshold
+            break;
+        end
+    end
 end
