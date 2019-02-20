a7d8980606eb264eefa5203314017e22720e4ce6
Momchil Tomov <tomov90@gmail.com>
2019-02-19 22:06:23 -0500

    figure

--- demo.m
+++ demo.m
@@ -6,7 +6,8 @@ w_test = {[1 1 0], [0 0 1]};
 
 %for iter = 1:10
 
-[pi_test_UVFA, pi_test_SF, V, V_test, psi, Vmax] = sim(env, w_train, w_test);
+%[pi_test_UVFA, pi_test_SF, V, V_test, psi, Vmax] = sim(env, w_train, w_test);
+load demo.mat
 
 for t = 1:length(w_test)
 
@@ -38,3 +39,35 @@ for t = 1:length(w_test)
 end
 
 %end
+
+figure;
+
+% plot performance on test tasks
+for t = 1:length(w_test)
+    subplot(2, length(w_test), t);
+    bar([1 2], tot_r(t,:));
+    xticklabels({'UVFA', 'SF'});
+    ylabel('total reward');
+    title(sprintf('test w = [%.1f %.1f %.1f]', w_test{t}));
+end
+
+% plot MDP as graph
+E = zeros(env.N, env.N);
+for s = 1:env.N
+    for s_new = 1:env.N
+        if sum(squeeze(env.T(s,:,s_new))) > 0
+            E(s, s_new) = 1;
+        end
+    end
+end
+G = digraph(E);
+
+subplot(2, 1, 2);
+h = plot(G);
+for s = 1:env.N
+    labelnode(h, s, sprintf('phi(%d) = [%.1f %.1f %.1f]', s, env.phi{s}));
+end
+ylim([-0.5 4]);
+xlim([0.5 11]);
+set(gca, 'xtick', []);
+set(gca, 'ytick', []);
