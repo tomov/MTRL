34401a0b5c7232939afd8f3fb1b991e4a857c6ca
Momchil Tomov <tomov90@gmail.com>
2019-03-25 21:16:16 -0400

    refactor demo_v1_1

--- demo_v1_1.m
+++ demo_v1_1.m
@@ -7,155 +7,115 @@ filename = 'demo_v1_1c.mat';
 w_train = {[1 0 0 0], [0 1 0 0], [1 0 0 0], [0 1 0 0]};
 w_test = {[1 1 0 0], [0 0 1 0], [0 0 0 1]}; 
 params = init_params();
+N = 60;
 
-for iter = 1:60
+%
+% train models
+%
 
-    UVFA = train_UVFA(env, w_train, params.gamma, 100);
-    pi_test_UVFA = test_UVFA(env, w_test, params.gamma, UVFA);
+for subj = 1:N
 
-    psi = train_SFGPI(env, w_train, params.gamma);
-    pi_test_SF = test_SFGPI(env, w_test, params.gamma, psi);
+    UVFA{subj} = train_UVFA(env, w_train, params.gamma, 100);
 
-    pi_test_MB = test_MB(env, w_test, params.gamma);
+    psi{subj} = train_SFGPI(env, w_train, params.gamma);
 
-    Q = train_MF(env, w_train, params.gamma, params.alpha, params.eps);
-    pi_test_MF = test_MF(env, w_test, Q);
-
-    for t = 1:length(w_test)
-
-        % test UVFA
-        [r, s] = test_perf(env, pi_test_UVFA{t}, w_test{t});
-        term(t, 1, iter) = s;
-        tot_r(t, 1, iter) = r;
-
-        % test SF 
-        [r, s] = test_perf(env, pi_test_SF{t}, w_test{t});
-        term(t, 2, iter) = s;
-        tot_r(t, 2, iter) = r;
-
-        % test MB 
-        [r, s] = test_perf(env, pi_test_MB{t}, w_test{t});
-        term(t, 3, iter) = s;
-        tot_r(t, 3, iter) = r;
-
-        % test MF 
-        [r, s] = test_perf(env, pi_test_MF, w_test{t});
-        term(t, 4, iter) = s;
-        tot_r(t, 4, iter) = r;
-    end
+    Q{subj} = train_MF(env, w_train, params.gamma, params.alpha, params.eps);
 
 end
 
 
 save(filename);
 
-%load(filename);
-
-figure;
-
-sem = @(x) std(x) / sqrt(length(x));
-
-% plot performance on test tasks, fig for each test task
-for t = 1:length(w_test)
-    subplot(3, length(w_test), t);
-
-    clear m;
-    clear se;
-    for i = 1:4
-        r = squeeze(tot_r(t, i, :));
-        m(i) = mean(r);
-        se(i) = sem(r);
-    end
-
-    hold on;
-    bar([1:4], m);
-    errorbar([1:4], m, se, 'color', [0 0 0], 'linestyle', 'none');
-    xticks([1:4]);
-    xticklabels({'UVFA', 'SF&GPI', 'MB', 'MF'});
-    ylabel('test reward');
-    title(sprintf('test w = [%.0f %.0f %.0f %.0f]', w_test{t}));
-end
-
-% plot performance on test tasks, fig for each algo
-labels = {};
-for t = 1:length(w_test)
-    labels{t} = sprintf('w = [%.0f %.0f %.0f %.0f]', w_test{t});
-end
-titles = {'UVFA', 'SF&GPI', 'MB', 'MF'};
-for i = 1:4
-    subplot(3, 4, 4 + i);
-
-    clear m;
-    clear se;
-    for t = 1:length(w_test)
-        r = squeeze(tot_r(t, i, :));
-        m(t) = mean(r);
-        se(t) = sem(r);
-    end
-
-    hold on;
-    bar(m);
-    errorbar(m, se, 'color', [0 0 0], 'linestyle', 'none');
-    xticks(1:length(m));
-
-    xticklabels(labels);
-    xtickangle(40);
-    ylabel('test reward');
-    title(titles{i});
-    xlabel('test task');
-end
-
-% plot MDP as graph
-E = zeros(env.N, env.N);
-for s = 1:env.N
-    for s_new = 1:env.N
-        if sum(squeeze(env.T(s,:,s_new))) > 0
-            E(s, s_new) = 1;
-        end
-    end
-end
-G = digraph(E);
-
-subplot(3, 1, 3);
-h = plot(G);
-for s = 1:env.N
-    labelnode(h, s, sprintf('phi(%d) = [%.0f %.0f %.0f %.0f]', s, env.phi{s} * 10));
-end
-ylim([-0.5 4]);
-xlim([0.5 11]);
-set(gca, 'xtick', []);
-set(gca, 'ytick', []);
-title('MDP');
-xlabel('note: all features x 10 (for vizualization)');
 
 
-% histogram of final test states
+%
+% eval perf on train tasks
 %
 
-figure;
+for subj = 1:N
 
-plot_idx = 0;
-for i = 1:4
+    % compute test policies
+    pi_train_UVFA = test_UVFA(env, w_train, params.gamma, UVFA{subj});
+    pi_train_SF = test_SFGPI(env, w_train, params.gamma, psi{subj});
+    pi_train_MB = test_MB(env, w_train, params.gamma);
+    pi_train_MF = test_MF(env, w_train, Q{subj});
+
+    for t = 1:length(w_train)
+
+        % test UVFA
+        [r, s] = test_perf(env, pi_train_UVFA{t}, w_train{t});
+        term_s_train(t, 1, subj) = s;
+        tot_r_train(t, 1, subj) = r;
+
+        % test SF 
+        [r, s] = test_perf(env, pi_train_SF{t}, w_train{t});
+        term_s_train(t, 2, subj) = s;
+        tot_r_train(t, 2, subj) = r;
+
+        % test MB 
+        [r, s] = test_perf(env, pi_train_MB{t}, w_train{t});
+        term_s_train(t, 3, subj) = s;
+        tot_r_train(t, 3, subj) = r;
+
+        % test MF 
+        [r, s] = test_perf(env, pi_train_MF, w_train{t});
+        term_s_train(t, 4, subj) = s;
+        tot_r_train(t, 4, subj) = r;
+    end
+
+end
+
+
+%
+% eval perf on test tasks
+%
+
+for subj = 1:N
+
+    % compute test policies
+    pi_test_UVFA = test_UVFA(env, w_test, params.gamma, UVFA{subj});
+    pi_test_SF = test_SFGPI(env, w_test, params.gamma, psi{subj});
+    pi_test_MB = test_MB(env, w_test, params.gamma);
+    pi_test_MF = test_MF(env, w_test, Q{subj});
 
     for t = 1:length(w_test)
-        
-        cnt = [];
-        for s = 5:13 % TODO hardcoded
-            which = squeeze(term(t, i, :)) == s;
-            cnt(s) = sum(which);
-        end
 
-        plot_idx = plot_idx + 1;
-        subplot(4, length(w_test), plot_idx);
+        % test UVFA
+        [r, s] = test_perf(env, pi_test_UVFA{t}, w_test{t});
+        term_s_test(t, 1, subj) = s;
+        tot_r_test(t, 1, subj) = r;
 
-        bar(cnt);
+        % test SF 
+        [r, s] = test_perf(env, pi_test_SF{t}, w_test{t});
+        term_s_test(t, 2, subj) = s;
+        tot_r_test(t, 2, subj) = r;
 
-        title(labels{t});
-        xticks(5:13);
-        xlim([4.5 13.5]);
-        xlabel('final state');
-        if t == 1
-            ylabel(titles{i});
-        end
+        % test MB 
+        [r, s] = test_perf(env, pi_test_MB{t}, w_test{t});
+        term_s_test(t, 3, subj) = s;
+        tot_r_test(t, 3, subj) = r;
+
+        % test MF 
+        [r, s] = test_perf(env, pi_test_MF, w_test{t});
+        term_s_test(t, 4, subj) = s;
+        tot_r_test(t, 4, subj) = r;
     end
 end
+
+save(filename);
+
+
+%load(filename);
+
+model_names = {'UVFA', 'SF&GPI', 'MB', 'MF'};
+
+%plot_perf(env, w_train, tot_r_train, model_names); % <-- boring
+
+plot_final_states(env, w_train, term_s_train, model_names);
+
+
+
+
+plot_perf(env, w_test, tot_r_test, model_names);
+
+plot_final_states(env, w_test, term_s_test, model_names);
