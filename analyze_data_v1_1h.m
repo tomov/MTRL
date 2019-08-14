b13388d3404519b81969926d51727e9fced729ac
Momchil Tomov <tomov90@gmail.com>
2019-08-14 11:05:40 -0400

    j

--- analyze_data_v1_1h.m
+++ analyze_data_v1_1h.m
@@ -4,7 +4,8 @@ clear all;
 
 %[data, Ts, ~, durs, ~, ~, avg_rew, filenames] = load_data('exp/results/usfa_v1_1h_long', 201);
 %[data, Ts, ~, durs, ~, ~, avg_rew, filenames] = load_data('exp/results/usfa_v1_1h', 101);
-[data, Ts, ~, durs, ~, ~, avg_rew, filenames] = load_data('exp/results/usfa_v1_1i', 101);
+%[data, Ts, ~, durs, ~, ~, avg_rew, filenames] = load_data('exp/results/usfa_v1_1i', 101);
+%[data, Ts, ~, durs, ~, ~, avg_rew, filenames] = load_data('exp/results/usfa_v1_1j', 101);
 %[data, Ts, ~, durs, ~, ~, avg_rew, filenames] = load_data('exp/results/usfa_v1_1h_batch2', 101);
 %[data, Ts, ~, durs] = load_data('exp/results/usfa_v1_1g_long', 119);
 load data.mat
@@ -12,9 +13,11 @@ load data.mat
 %data = data(durs < 50, :);
 
 
-which = avg_rew >= median(avg_rew);
+%{
+which = avg_rew < 0; %median(avg_rew);
 data = data(which, :);
 filenames = filenames(which);
+%}
 
 sem = @(x) std(x) / sqrt(length(x));
 
