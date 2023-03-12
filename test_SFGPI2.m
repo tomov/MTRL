227bd5b2c774bd040385122c3facf2b8075cce6b
Momchil Tomov <tomov90@gmail.com>
2023-03-12 13:53:45 -0400

    test_SFGPI2.m train_SFGPI2.m

---
+++ test_SFGPI2.m
@@ -0,0 +1,33 @@
+function [pi_test_SF] = test_SFGPI2(env, w_test, gamma, beta, psi)
+
+    % Following the equations more closely
+
+    % see what SF will do 
+    for t = 1:length(w_test)
+        % compute Q (eq. 7 from Barreto et al. 2020 PNAS)
+        clear Q;
+        for tt = 1:length(psi) % loop over training tasks
+            Q{tt} = nan(env.N, length(env.A));
+            for s = 1:env.N
+                for a = 1:length(env.A)
+                    Q{tt}(s,a) = psi{tt}{s,a} * w_test{t}';
+                end
+            end
+        end
+        
+        % compute policies
+        Qmax = -inf(env.N,length(env.A));
+        for s = 1:env.N
+            for a = env.A
+                for tt = 1:length(psi) % loop over training tasks
+                    Qmax(s,a) = max(Qmax(s,a), Q{tt}(s,a)); % eq. 8 from PNAS paper
+                end
+            end
+
+            P = exp(Qmax(s,:) * beta);
+            P = P / sum(P);
+            pi_test_SF{t}{s} = P;
+        end
+    end
+
+
---
+++ train_SFGPI2.m
@@ -0,0 +1,52 @@
+function [psi] = train_SFGPI2(env, w_train, gamma, beta, threshold)
+
+    if ~exist('threshold', 'var')
+        threshold = 0.01;
+    end
+
+    % value iteration -- V{t}(s) = value f'n for task t, state s
+    %
+    for t = 1:length(w_train)
+        %fprintf('t = %d\n', t);
+
+        [V{t}, pi{t}] = value_iteration(env, w_train{t}, gamma, beta);
+    end
+
+    % get SFs using iteration
+
+    for t = 1:length(w_train)
+        %fprintf('t = %d\n', t);
+
+        % important to init at least terminal states
+        for s = 1:env.N
+            for a = env.A
+                psi{t}{s,a} = env.phi{s};
+            end
+        end
+
+        while true
+            delta = 0;
+            for s = env.S
+                for a = env.A
+                    fprintf('   s = %d, a = %d\n', s, a);
+                    old = psi{t}{s,a};
+
+                    psi{t}{s,a} = env.phi{s};
+                    for s_new = 1:env.N
+                        assert(abs(sum(pi{t}{s}) - 1) < 1e-12);
+                        for a_new = env.A
+                            psi{t}{s,a} = psi{t}{s,a} + pi{t}{s}(a) * env.T(s, a, s_new) * gamma * pi{t}{s_new}(a_new) * psi{t}{s_new,a_new};
+                            fprintf('          s_new = %d, a_new = %d, T(s,a,s_new) = %f, psi{t}{s_new,a_new} = [%f %f %f]\n', s_new, a_new, env.T(s, a, s_new), psi{t}{s_new,a_new});
+                        end
+                    end
+                    delta = max(delta, norm(old - psi{t}{s,a}));
+                end
+
+            end
+
+            if delta < threshold
+                break;
+            end
+        end
+    end
+
